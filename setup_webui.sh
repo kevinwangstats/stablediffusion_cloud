ca6c92a02d69566f5658759aac84c0d8c7ae76b4
Kevin Wang <92612961+kevinwangstats@users.noreply.github.com>
2023-11-13 17:26:28 +1100

    upd set up script

--- setup_webui.sh
+++ setup_webui.sh
@@ -1,13 +1,35 @@
+#!/bin/bash
+
+# WORKING_DIR=$HOME/projects
+WORKING_DIR=$HOME
 
 sudo apt-get update
-sudo apt install -y wget git python3 python3-venv libgl1 libglib2.0-0 
+sudo apt install -y wget git python3 python3-venv libgl1 libglib2.0-0
 
+PYTHON3_PATH=$(which python3)
+PIP3_PATH=$(which pip3)
+pip3 install opencv-python moviepy pillow
+
+## Install miniconda3
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# bash ~/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+# source $HOME/miniconda3/etc/profile.d/conda.sh
+# conda activate base
+
+cd $WORKING_DIR
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
-cd stable-diffusion-webui
+SDUI_DIR=$WORKING_DIR/stable-diffusion-webui
 
-bash ~/stable-diffusion-webui/webui.sh
+## Download ControlNet QR Code model
+mkdir $SDUI_DIR/models/ControlNet
+wget -P $SDUI_DIR/models/ControlNet https://civitai.com/api/download/models/122143 --content-disposition
 
-## Install xformers
+## Download Landscape stable-diffusion model
+wget -P $SDUI_DIR/models/Stable-diffusion https://civitai.com/api/download/models/148587 --content-disposition
+
+bash $SDUI_DIR/webui.sh
+
+## Install xformers (takes a very long time)
 # source ./venv/bin/activate
 # cd repositories
 # git clone https://github.com/facebookresearch/xformers.git
@@ -16,25 +38,8 @@ bash ~/stable-diffusion-webui/webui.sh
 # pip install -r requirements.txt
 # pip install -e .
 
-## Install miniconda3
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# bash ~/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
-# source $HOME/miniconda3/etc/profile.d/conda.sh
-# conda activate base
-
-## Install NVIDIA 
-# wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
-# sudo sh cuda_11.8.0_520.61.05_linux.run
-
-## Run this locally to do port forwarding
+## Run this locally to do port forwarding, so the SD UI runs locally at localhost:7860
 # gcloud compute ssh --project=voice-387504 --zone=us-west3-b kevinwangstats@instance-1 -- -L 7860:localhost:7860
-
-## Download ControlNet QR Code model
-# mkdir ~/stable-diffusion-webui/models/ControlNet
-# wget -P ~/stable-diffusion-webui/models/ControlNet https://civitai.com/api/download/models/122143 --content-disposition
-
-## Download Stable-diffusion model
-# wget -P ~/stable-diffusion-webui/models/Stable-diffusion https://civitai.com/api/download/models/148587 --content-disposition
 
 ## Run this to transfer images out
 # gcloud compute scp --project=voice-387504 --zone=us-west3-b kevinwangstats@instance-1:~/stablediffusion_cloud/img ~/Desktop
